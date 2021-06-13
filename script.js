75f6c45411fc4c64a86147b9a0374504478035c4
Cristina Lorenzo <cristina@numericco.com>
2021-06-13 22:51:02 +0200

    dynamic profile cards

--- script.js
+++ script.js
@@ -7,10 +7,48 @@ async function getUser(username) {
     try {
         const {data} = await axios(APIURL + username);
 
-        console.log(data);
+        createUserCard(data);
     } catch (err) {
-        console.log(err);
+        if(err.response.status == 404) {
+            createErrorCard('No hay nadie con este nombre de usuario');
+        }
     }
+}
+
+function createUserCard(user) {
+    const cardHTML = `
+    <div class="card">
+        <div>
+            <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+        </div>
+        <div class="user-info">
+            <h2>${user.name}</h2>
+            <p>${user.bio}</p>
+        
+            <ul>
+                <li>${user.followers} <strong>Seguidores</strong></li>
+                <li>${user.following} <strong>Seguidos</strong></li>
+                <li>${user.public_repos} <strong>Repos</strong></li>
+            </ul>
+
+            <div class="repos">
+
+            </div>
+        </div>
+    </div>
+    `;
+
+    main.innerHTML = cardHTML;
+}
+
+function createErrorCard(msg) {
+    const cardHTML = `
+        <div class=""card>
+            <h1>${msg}</h1>
+        </div>
+    `;
+
+    main.innerHTML = cardHTML;
 }
 
 form.addEventListener('submit', (e) => {
